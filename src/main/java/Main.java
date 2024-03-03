658b828bb9773281ae6507df15e80dc69892bdcf
LydiaStoneKonstanski <yourname@example.com>
2024-03-03 10:41:13 -0500

    finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,27 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  * Modified by kristofer on Jan 2024.
  */
 public class Main {
+    // Create a Scanner object to read input from the console
+    static String a = "Alice";
+    static String b = "Bob";
+    static Scanner scanner = new Scanner(System.in);
+//compute
+    public static void main(String[] args){
 
-    public static void main(String[] args ){
-        // create a new object of class Main
+        System.out.print("Input your full name: ");
+        String yourName = scanner.nextLine();
 
-        // call the compute() method on that new object
-    }
+        if (Objects.equals(yourName, a))
+            System.out.println("Hello " + yourName);
 
-    public void compute() {
-        // Get input string of name from user
-        
-        // Create conditional to check name against Alice and Bob
+        else if (Objects.equals(yourName, b))
+            System.out.println("Hello " + yourName);
 
-        // Print greeting to screen if Alice or Bob are true
-        
-        // Print "You are neither Alice nor Bob." otherwise
-
+        else System.out.println("You are neither Alice nor Bob.");
     }
 }
